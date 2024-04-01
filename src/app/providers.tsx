7dd2e68e1e50a7c3018508fb573a2eed93c1c92c
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2024-04-01 21:39:12 +0200

    first commit

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+'use client';
+//  ti share state:
+import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
+
+interface ProvidersProps {
+    children: React.ReactNode;
+}
+export default function Providers({ children }: ProvidersProps) {
+    return (
+        <SessionProvider>
+            <NextUIProvider>{children}</NextUIProvider>
+        </SessionProvider>
+    );
+}
---
+++ src/auth.ts
@@ -0,0 +1,38 @@
+import NextAuth from 'next-auth';
+import Github from '@auth/core/providers/github';
+//   notes on PrismaAdapter - when user registers, PA reaches out to schema
+//  to create a user based on that schema
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from '@/db';
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+    throw new Error('Missing github oauth credentials');
+}
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signOut,
+    signIn,
+} = NextAuth({
+    adapter: PrismaAdapter(db),
+    providers: [
+        Github({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+        }),
+    ],
+    callbacks: {
+        //  usually not needed
+        async session({ session, user }: any) {
+            if (session && user) {
+                session.user.id = user.id;
+            }
+
+            return session;
+        },
+    },
+});
